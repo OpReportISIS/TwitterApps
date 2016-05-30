04e9c224b26a2697777a0abb8ada78074743fdb6
OpReportISIS <pswaux920@tutanota.com>
2016-05-30 17:38:04 +0800

    Create config.py

---
+++ TwitterIDConverter/config.py
@@ -0,0 +1,9 @@
+#----------------------------------------------------------------------------
+# OAuth client credentials can be generated via Twitter's Application Management:
+# https://apps.twitter.com/app/new
+#----------------------------------------------------------------------------
+
+consumer_key = "XxXxXxxXXXxxxxXXXxXX"
+consumer_secret = "xXXXXXXXXxxxxXxXXxxXxxXXxXxXxxxxXxXXxxxXXx"
+access_token = "XXXXXXXX-xxXXxXXxxXxxxXxXXxXxXxXxxxXxxxxXxXXxXxxXX"
+access_token_secret = "XxXXXXXXXXxxxXXXxXXxXxXxxXXXXXxXxxXXXXx"
